a1722e8047c9013acc4bc9bbca2dbef04d76a934
AlexandreMPDias <alexandrempdias@icloud.com>
2023-05-29 14:33:14 -0300

    safety check result

--- src/ui/containers/with-safety-check/components/safety-check-result.tsx
+++ src/ui/containers/with-safety-check/components/safety-check-result.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Newline, Text } from 'ink';
+import { Text } from 'ink';
 import { TextBox } from '../../../components/text-box/index.js';
 import { ISafetyCheckProps } from './safety-check.js';
 
@@ -7,18 +7,28 @@ export interface ISafetyCheckResultProps extends ISafetyCheckProps {
 	readonly userGaveConsent: boolean;
 }
 
+const RunOnShell: React.FC<{ label: string; command: string }> = (props) => {
+	return (
+		<>
+			<Text color="red">{'> ' + props.label}</Text>
+			<TextBox color="red" alignItems="flex-start" alignSelf="flex-start">
+				{props.command}
+			</TextBox>
+		</>
+	);
+};
+
 const ShellExportEnv: React.FC = () => {
 	if (process.platform === 'win32') {
 		return (
-			<TextBox color="red">
-				{`on Powershell: $env:SECRETS_ENV='local'`}
-				<Newline />
-				{`on CMD: set "SECRETS_ENV=local"`}
-			</TextBox>
+			<>
+				<RunOnShell label="Run on Powershell:" command={`$env:SECRETS_ENV='local'`} />
+				<RunOnShell label="Run on CMD:" command={`set "SECRETS_ENV=local"`} />
+			</>
 		);
 	}
 	if (process.platform === 'linux' || process.platform === 'darwin') {
-		return <TextBox color="red">{`SECRETS_ENV="local"`}</TextBox>;
+		return <RunOnShell label="Run on your terminal:" command={`SECRETS_ENV="local"`} />;
 	}
 	return (
 		<TextBox color="red">
@@ -41,8 +51,7 @@ export const SafetyCheckResult: React.FC<ISafetyCheckResultProps> = (props) => {
 	return (
 		<>
 			<Text color="red">{'Stopping execution.'}</Text>
-			<Newline />
-			<Text color="red">{`If you wanted to run on [local] environment, run:`}</Text>
+			<Text color="red">{`If you wanted to execute using [local] environment:`}</Text>
 			<ShellExportEnv />
 		</>
 	);
